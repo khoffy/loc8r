aca9ea71530e5fce075ff0860622aa06040ebd2e
khoffy <khoffy.tipoh@gmail.com>
2021-10-20 23:36:53 +0000

    Defining routes for different screens

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const ctrlMain = require('../controllers/main');
+const ctrlLocations = require('../controllers/locationsController');
+const ctrlOthers = require('../controllers/othersController');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocation.locationInfo);
+router.get('/location/review/new', ctrlLocation.addReview);
+
+/* Other page */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
