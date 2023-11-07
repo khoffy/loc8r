0ee7ba6686e60164fb7128b249d3f13c99f225ea
khoffy <adolf_hermann86@yahoo.fr>
2023-11-06 22:25:35 -0600

    Moving out the data from the views and set them in the controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,36 @@
 const homelist = (req, res) => {
-    res.render('locations-list', {title: 'Home'})
+    res.render('locations-list', {
+        title: 'Loc8r - Find a place to work with wifi',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: ' - Find places to work with wifi near you!'
+        },
+        sidebar: 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about. ' +  
+            'Perhaps with coffee, acke or a pint? Let Loc8r help you find the place you\'re looking for.',
+        locations: [
+            {
+                name: 'Starcups',
+                address: '1000 North 4th Street',
+                rating: 4,
+                facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+                distance: '175m'
+            },
+            {
+                name: 'Caffe Hero',
+                address: '406 N D Street',
+                rating: 2,
+                facilities: ['Food', 'Premium wifi'],
+                distance: '200m'
+            },
+            {
+                name: 'Paradiso',
+                address: '304 North Carpenter Ave',
+                rating: 3,
+                facilities: ['Food', 'Netflix', 'Premium wifi'],
+                distance: '250m'
+            }
+        ]
+    })
 }
 
 const locationInfo = (req, res) => {
